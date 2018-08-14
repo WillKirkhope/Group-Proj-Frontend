f058f514ae1c366ea4a5bf08e5bda9836de37930
WillKirkhope <kirkhopetraining@yahoo.com>
2018-08-14 10:45:25 -0600

    Added Vue Init files and made changes to Vue app.vue as well as added some components with simple styling

---
+++ openvillage/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Header />
+    <Dummy />
+    <Footer />
+  </div>
+</template>
+
+
+
+<script>
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Dummy from './components/Dummy'
+
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer,
+    Dummy
+  }
+}
+</script>
+
+
+
+<style>
+
+</style>
---
+++ openvillage/src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <img src="https://villageidiotblog.files.wordpress.com/2013/08/villageidiot_sign.jpg" alt="placeholder">
+    <nav>
+      <a href="#">Home</a>
+      <a href="#">Events</a>
+      <a href="#">About</a>
+      <a href="#">Login</a>
+    </nav>
+  </header>
+
+</template>
+
+
+<script>
+  export default {
+    name: 'Header'
+  }
+</script>
+
+
+<style scoped>
+  header {
+    display: flex;
+    justify-content: space-between;
+    background-color: beige;
+    height: 20vw;
+    width: 100vw;
+  }
+  img {
+    width: 40vw;
+    height: auto;
+  }
+</style>
