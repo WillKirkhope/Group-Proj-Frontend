444e48176e396c33e4f0fcfd75291a48605b3848
Josh Robbs <joshrobbs@Zuul.local>
2018-08-15 19:25:21 -0600

    form is posting to database but not reloading on redirect to home

--- openvillage/src/components/Form.vue
+++ openvillage/src/components/Form.vue
@@ -1,92 +1,100 @@
 <template>
-<div class = "form-container">
-    <form id = "form">
+  <div class = "form-container">
+    <form id="form" @submit.prevent="postNewEvent">
       <div class = "form-div">
-      <label htmlFor="name">Organizer</label>
-      <input type="text" name="name" />
+        <label htmlFor="name">Organizer</label>
+        <input type="integer" name="organizer" v-model="formData.organizer" />
       </div>
       <div class = "form-div">
-      <label htmlFor="Event Name">Event Name</label>
-      <input type="text" name="Event Name" />
+        <label htmlFor="eventName">Event Name</label>
+        <input type="text" name="eventName" v-model="formData.name" />
       </div>
       <div class = "form-div">
-      <label htmlFor="Latitude">Latitude</label>
-      <input type="integer" name="Latitude" />
+        <label htmlFor="lat">Latitude</label>
+        <input type="integer" name="lat" v-model="formData.lat" />
       </div>
       <div class = "form-div">
-      <label htmlFor="Longitude">Longitude</label>
-      <input type= "integer" name="Longitude" />
+        <label htmlFor="long">Longitude</label>
+        <input type= "integer" name="long" v-model="formData.long" />
       </div>
       <div class = "form-div">
-      <label htmlFor="Description">Description</label>
-      <textarea type="text" name="Description" ></textarea>
+        <label htmlFor="description">Description</label>
+        <textarea type="text" name="description" v-model="formData.description">My event is...</textarea>
       </div>
-      <input id = "submit-button" type="submit" name="submit" value="Submit" />
+      <input id="submit-button" type="submit" name="submit" value="Submit" />
     </form>
-    </div>
+  </div>
 </template>
 
 
 <script>
 export default {
   name: 'Form',
-  components: {
-  },
   data(){
     return {
-      formData: {}
+      formData: {
+        organizer: '',
+        name: '',
+        lat: '',
+        long: '',
+        description: ''
+      }
     }
   },
   methods: {
     testFunction(){
       console.log('hello from methods!')
+    },
+    mounted() {
+      this.testFunction()
+    },
+    postNewEvent(){
+      console.log("i was submitted?",this.formData)
+      fetch('https://openvillage.herokuapp.com/events', {
+        method: 'POST',
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(this.formData)
+      })
+        .then(window.location.replace("http://localhost:8080/#/").reload)
     }
-  },
-  mounted: function () {
-    this.testFunction()
   }
 }
+
 </script>
 
 
 <style>
- 
- #form {
-   display:flex;
-   flex-direction:column;
-   padding:50px;
-   width:40vw;
-   align-items:center;
- }
 
- .form-div {
-   padding:20px;
- }
+#form {
+  display:flex;
+  flex-direction:column;
+  padding:50px;
+  width:40vw;
+  align-items:center;
+}
 
- input {
+.form-div {
+  padding:20px;
+}
+
+input {
   width: 40vw;
- }
+}
 
- textarea {
-   width: 40vw;
- }
+textarea {
+  width: 40vw;
+}
 
- .form-container {
-   display:flex;
-   justify-content:center;
-   background-color: rgb(228, 230, 234);
+.form-container {
+  display:flex;
+  justify-content:center;
+  background-color: rgb(228, 230, 234);
 
- }
+}
 
- #submit-button {
-   padding: 1vw;
-   width: 20vw;
- }
-
-
-
-
-
-
+#submit-button {
+  padding: 1vw;
+  width: 20vw;
+}
 
 </style>
